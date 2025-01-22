c1c71e06de97800903dcf540dd94255292a2bed4
Emilie Resca <emilie.resca@mymail.champlain.edu>
2025-01-22 14:04:29 -0500

    test commit

---
+++ week2/Untitled1.ps1
@@ -0,0 +1,3 @@
+﻿
+Get-Service | Where-Object {$_.Status -eq "Stopped"} | `
+    Sort-Object name | Export-Csv -Path "C:\Users\champuser\SYS320-01\week2\stopped_services.csv" 
---
+++ week2/chrome-start-stop.ps1
@@ -0,0 +1,7 @@
+﻿#is chrome running? if so, end it, if not, start it
+
+
+if (Get-Process -Name "chrome" -ErrorAction SilentlyContinue) {
+    Stop-Process -Name chrome
+} else { Start-Process "chrome.exe"
+}
