856f2cd63060ab8b850d8791234d9d0764fa10f7
Emilie Resca <emilie.resca@mymail.champlain.edu>
2025-02-05 12:54:36 -0500

    week 3 commit

---
+++ week 3/functions_logs.ps1
@@ -0,0 +1,40 @@
+﻿#
+#Get-EventLog -LogName System -source Microsoft-Windows-Winlogon
+
+#
+function numberofdays($days){
+
+$days = Read-Host "Enter the number of days you want to obtain logs from"
+$loginoutsTable = numberofdays $days
+
+# get login and log off for last 14 days and save to a variable
+$loginouts = Get-EventLog -LogName System -source Microsoft-Windows-Winlogon -After (Get-Date).AddDays(-$days)
+$loginoutsTable = @()
+for( $i = 0; $i -lt $loginouts.count; $i++){
+
+#event property value
+$event = ""
+if($loginouts[$i].EventID -eq '7001') {$event="Logon"}
+if($loginouts[$i].EventID -eq '7002') {$event="Logoff"}
+
+#user property value
+$user = $loginouts[$i].ReplacementStrings[1]
+
+# translate user id to usernmae
+$test= New-Object System.Security.Principal.SecurityIdentifier $user
+$username= $test.Translate([System.Security.Principal.NTAccount])
+
+#add each line to empty array
+$loginoutsTable += [pscustomobject]@{"Time" = $loginouts[$i].TimeGenerated;
+"Id" = $loginouts[$i].EventID
+"Event" = $event;
+"User" = $username;
+}
+$days = Read-Host "Enter the number of days you want to obtain logs from"
+$loginoutsTable = numberofdays $days
+}
+
+$loginoutsTable
+}
+
+
