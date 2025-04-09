4a71fc19fb2aa31bcf1188ea64215a322017ac78
Emilie Resca <you@example.com>
2025-04-09 13:33:50 -0400

    what the Helly E.

---
+++ week11/basic_access_intruder.bash
@@ -0,0 +1,4 @@
+for a in {1..20}
+do
+curl 10.0.17.38
+done
---
+++ week11/lecture.bash
@@ -0,0 +1,19 @@
+#!bin/bash
+allLogs=""
+file="/var/log/apache2/access.log"
+
+
+function GetAllLogs(){
+allLogs=$(cat "$file" | cut -d' ' -f1,4,7 | tr -d "[")
+}
+
+function ips(){
+ipsAccessed=$(echo "$allLogs" | cut -d' ' -f1)
+}
+
+function pageCount(){
+pagesAmount=$(cat "$file" | grep "curl/" | cut -d' ' -f1,12 | sort | uniq -c)
+}
+
+pageCount
+echo "$pagesAmount"
